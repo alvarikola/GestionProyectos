4c7d73d209344df728e153ef599a1983c5493462
alvarikola <alvaroe484@gmail.com>
2025-02-04 10:30:54 +0000

    creado modelo

---
+++ src/main/kotlin/modelo.kt
@@ -0,0 +1,10 @@
+data class Proyecto(
+    val id: Int,
+    val nombre: String,
+    val descripcion: String,
+    val fecha_creacion: String,
+    val fecha_inicio: String,
+    val fecha_finalizacion: String?,
+    val cliente: Int
+)
+
