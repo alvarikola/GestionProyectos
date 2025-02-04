7f8cbae00bb517b27a7171a44daa6560e18ba0d4
alvarikola <alvaroe484@gmail.com>
2025-02-04 11:47:15 +0000

    creado tareaScreen

---
+++ src/main/kotlin/Screen/TareaScreen.kt
@@ -0,0 +1,12 @@
+package Screen
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import modelo.Tarea
+
+class TareaScreen(tarea: Tarea): Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
