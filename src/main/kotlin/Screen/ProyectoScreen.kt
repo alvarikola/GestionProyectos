675fcfe1c0e46ec3f433b20c605f63869bd9ea78
alvarikola <alvaroe484@gmail.com>
2025-02-07 09:57:38 +0000

    obtener tareas de la API

--- src/main/kotlin/Screen/ProyectoScreen.kt
+++ src/main/kotlin/Screen/ProyectoScreen.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import modelo.Proyecto
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,24 +20,21 @@ import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import modelo.Tarea
+import network.apiObtenerTareas
 import theme.*
 
 class ProyectoScreen(proyecto: Proyecto): Screen {
-
-    val tareas = listOf(
-        Tarea(1,"Tarea1", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 1),
-        Tarea(2,"Tarea2", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 1),
-        Tarea(3,"Tarea3", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 2),
-        Tarea(4,"Tarea4", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 2),
-        Tarea(5,"Tarea5", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 2),
-        Tarea(6,"Tarea6", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 3),
-        Tarea(7,"Tarea7", "es una tarea", 4, "20/1/2025", "20/1/2025", 1, 4),
-    )
 
     val proyectoSeleccionado = proyecto
 
     @Composable
     override fun Content() {
+
+        val tareasList = remember { mutableStateOf(emptyList<Tarea>()) }
+        apiObtenerTareas(proyectoSeleccionado.id) {
+            tareasList.value = it
+        }
+
         Column(modifier = Modifier.background(Color(backgroundLight)).fillMaxSize().padding(20.dp)) {
             Row(
                 modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(7.dp)).background(Color(foregroundDark)).padding(20.dp),
@@ -56,16 +53,18 @@ class ProyectoScreen(proyecto: Proyecto): Screen {
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            LazyColumn(
-                modifier = Modifier
-            ) {
-                items(tareas) { tarea ->
-                    if (tarea.proyecto == proyectoSeleccionado.id) {
-                        TareaItem(tarea)
+            if (tareasList.value.isNotEmpty()) {
+                Text("Tareas", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+                LazyColumn(modifier = Modifier.padding(top = 5.dp)) {
+                    items(tareasList.value) {
+                        TareaItem(it)
                         Spacer(modifier = Modifier.height(10.dp))
                     }
                 }
             }
+
+            Text("Asignar", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+            DropdownMenuExample()
         }
 
     }
@@ -85,6 +84,7 @@ fun TareaItem(tarea: Tarea) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
+
         Column {
             Text(tarea.nombre)
             Text(tarea.descripcion)
@@ -97,6 +97,49 @@ fun TareaItem(tarea: Tarea) {
             colors = ButtonDefaults.buttonColors(backgroundColor = Color(backgroundDark))
         ) {
             Text("Ver tarea", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun DropdownMenuExample() {
+    // Estado para controlar la visibilidad del menú
+    val expanded = remember { mutableStateOf(false) }
+    val selectedOption = remember { mutableStateOf("Seleccione una opción") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        // Botón que muestra el menú desplegable
+        Button(onClick = { expanded.value = !expanded.value }) {
+            Text(text = "Tareas")
+        }
+
+        // Mostrar la opción seleccionada
+        Text(text = "Tarea a asignar: ${selectedOption.value}", modifier = Modifier.padding(top = 8.dp))
+
+        // Menú desplegable
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false }  // Cerrar el menú si se hace clic fuera
+        ) {
+            // Opciones del menú
+            DropdownMenuItem(onClick = {
+                selectedOption.value = "Opción 1"
+                expanded.value = false
+            }) {
+                Text("Opción 1")
+            }
+            DropdownMenuItem(onClick = {
+                selectedOption.value = "Opción 2"
+                expanded.value = false
+            }) {
+                Text("Opción 2")
+            }
+            DropdownMenuItem(onClick = {
+                selectedOption.value = "Opción 3"
+                expanded.value = false
+            }) {
+                Text("Opción 3")
+            }
         }
     }
 }
