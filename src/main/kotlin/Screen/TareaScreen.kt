e1876cc476c3d8bd308ef1c35bed1f4c15eb04e0
alvarikola <alvaroe484@gmail.com>
2025-02-09 16:22:58 +0000

    realizada pantalla de tarea

--- src/main/kotlin/Screen/TareaScreen.kt
+++ src/main/kotlin/Screen/TareaScreen.kt
@@ -1,12 +1,68 @@
 package Screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import modelo.Tarea
+import theme.backgroundLight
+import theme.foregroundDark
 
 class TareaScreen(tarea: Tarea): Screen {
+
+    val tareaSeleccionada = tarea
+
     @Composable
     override fun Content() {
+        Column(modifier = Modifier.background(Color(backgroundLight)).fillMaxSize().padding(20.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(7.dp)).background(Color(foregroundDark))
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column {
+                    Text(
+                        text = tareaSeleccionada.nombre,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    Text(
+                        text = tareaSeleccionada.descripcion,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                Column {
+                    Text(
+                        text = tareaSeleccionada.fecha_creacion,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                    tareaSeleccionada.fecha_finalizacion?.let {
+                        Text(
+                            text = it,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
 
+            Spacer(modifier = Modifier.height(20.dp))
+        }
     }
 }
